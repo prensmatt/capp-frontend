23f1d9f873d7b5fc3abecca64686136bc76dea6f
Maleyka Heybatova <6nisan2005@gmail.com>
2026-07-21 21:55:48 +0400

    login component ts, html, css

--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -1,9 +1,48 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { LoginRequest } from '../../../shared/models/models';
+import { AuthService } from '../../../core/services/auth.service';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule, RouterLink],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+
+
+export class LoginComponent {
+  credentials: LoginRequest={
+    email: '',
+    password: ''
+  };
+  error: string = '';
+  loading: boolean = false;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ){}
+
+  onSubmit():void{
+    this.loading = true;
+    this.error = ''
+
+    this.authService.login(this.credentials).subscribe({
+      next: () => {
+        if(this.authService.isAdmin()){
+          this.router.navigate(['/admin/products']);
+        } else{
+          this.router.navigate(['/products']);
+        }
+      },
+      error: ()=>{
+        this.error = 'Invalid email or password';
+        this.loading = false;
+      }
+    });
+  }
+}
