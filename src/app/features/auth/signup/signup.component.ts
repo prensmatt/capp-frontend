2613bf20411c8bda0befc7788346e5108f7ded8d
Maleyka Heybatova <6nisan2005@gmail.com>
2026-07-22 23:21:21 +0400

    signup component ts, html, css

--- src/app/features/auth/signup/signup.component.ts
+++ src/app/features/auth/signup/signup.component.ts
@@ -1,9 +1,44 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { SignupRequest } from '../../../shared/models/models';
+import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
   selector: 'app-signup',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule, RouterLink],
   templateUrl: './signup.html',
   styleUrl: './signup.css',
 })
-export class Signup {}
+export class Signup {
+  data: SignupRequest = {
+    name: '',
+    email: '',
+    password: '',
+    role: 'customer'
+  };
+  error: string = '';
+  loading: boolean = false;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ){}
+
+  onSubmit(): void{
+    this.loading = true;
+    this.error = '';
+  
+    this.authService.signup(this.data).subscribe({
+      next: () => {
+        this.router.navigate(['/login']);
+      },
+      error: ()=>{
+        this.error = 'Could not create account. Email may already be taken';
+        this.loading = false;
+      }
+    });
+  }
+}
