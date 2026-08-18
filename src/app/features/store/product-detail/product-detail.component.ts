d869699c0deaf15948fc083a0cffccf44e813b69
Maleyka Heybatova <6nisan2005@gmail.com>
2026-08-19 01:48:34 +0400

    product-detail component

--- src/app/features/store/product-detail/product-detail.component.ts
+++ src/app/features/store/product-detail/product-detail.component.ts
@@ -1,9 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+
+import { ProductService } from '../../../core/services/product.service';
+import { Product } from '../../../shared/models/models';
 
 @Component({
   selector: 'app-product-detail',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterLink],
   templateUrl: './product-detail.html',
-  styleUrl: './product-detail.css',
+  styleUrl: './product-detail.css'
 })
-export class ProductDetail {}
+export class ProductDetailComponent implements OnInit {
+  product: Product | null = null;
+  loading: boolean = false;
+  error: string = '';
+
+  constructor(
+    private productService: ProductService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    if (id) {
+      this.loading = true;
+      this.productService.getById(id).subscribe({
+        next: (data) => {
+          this.product = data;
+          this.loading = false;
+        },
+        error: () => {
+          this.error = 'Product not found';
+          this.loading = false;
+        }
+      });
+    }
+  }
+}
