c70733874b00482373af9b9d13d8467f8fd6cb57
Maleyka Heybatova <6nisan2005@gmail.com>
2026-08-19 17:19:14 +0400

    add navbar component

---
+++ src/app/shared/navbar/navbar.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink, Router } from '@angular/router';
+
+import { AuthService } from '../../core/services/auth.service';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [CommonModule, RouterLink],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.css'
+})
+export class NavbarComponent {
+  constructor(
+    public authService: AuthService,
+    private router: Router
+  ) {}
+
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
+}
